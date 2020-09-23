2908ace7b7dfd095f144dc5dec18512173f12826
Julian Thijssen <Nimthora@gmail.com>
2020-09-23 15:45:54 +0200

    Add ExtractFaceArea function

--- Source/FeatureExtraction.cpp
+++ Source/FeatureExtraction.cpp
@@ -1,23 +1,28 @@
 #include "FeatureExtraction.h"
 
+namespace
+{
+	float ExtractFaceArea(const Mesh& mesh, const Face& face)
+	{
+		const glm::vec3& v0 = mesh.positions[face.indices[0]];
+		const glm::vec3& v1 = mesh.positions[face.indices[1]];
+		const glm::vec3& v2 = mesh.positions[face.indices[2]];
+
+		glm::vec3 e1 = v1 - v0;
+		glm::vec3 e2 = v2 - v0;
+
+		float triangleArea = 0.5 * glm::length(glm::cross(e1, e2));
+		return triangleArea;
+	}
+}
+
 float ExtractSurfaceArea(Model& model)
 {
 	float totalSurfaceArea = 0;
+
 	for (Mesh& mesh : model.m_meshes)
-	{
 		for (Face& face : mesh.faces)
-		{
-			const glm::vec3& v0 = mesh.positions[face.indices[0]];
-			const glm::vec3& v1 = mesh.positions[face.indices[1]];
-			const glm::vec3& v2 = mesh.positions[face.indices[2]];
-
-			glm::vec3 e1 = v1 - v0;
-			glm::vec3 e2 = v2 - v0;
-
-			float triangleArea = 0.5 * glm::length(glm::cross(e1, e2));
-			totalSurfaceArea += triangleArea;
-		}
-	}
+			totalSurfaceArea += ExtractFaceArea(mesh, face);
 
 	return totalSurfaceArea;
 }
@@ -29,14 +34,7 @@ std::vector<double>& ExtractFaceAreas(Model& model)
 	{
 		for (Face& face : mesh.faces)
 		{
-			const glm::vec3& v0 = mesh.positions[face.indices[0]];
-			const glm::vec3& v1 = mesh.positions[face.indices[1]];
-			const glm::vec3& v2 = mesh.positions[face.indices[2]];
-
-			glm::vec3 e1 = v1 - v0;
-			glm::vec3 e2 = v2 - v0;
-
-			double triangleArea = 0.5 * glm::length(glm::cross(e1, e2));
+			double triangleArea = ExtractFaceArea(mesh, face);
 			facesAreas.push_back(triangleArea);
 		}
 	}
