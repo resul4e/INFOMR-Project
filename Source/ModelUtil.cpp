c5227cb8d94a43cb30167a6ade2d49ea9e0e6f59
Julian Thijssen <Nimthora@gmail.com>
2020-09-29 12:44:03 +0200

    Add Model Utility class for general computations on models

---
+++ Source/ModelUtil.cpp
@@ -0,0 +1,54 @@
+#include "ModelUtil.h"
+
+#include "Model.h"
+
+#include <glm/glm.hpp>
+
+namespace util
+{
+	glm::vec3 ComputeBarycenter(const Model& model)
+	{
+		glm::vec3 barycenter(0, 0, 0);
+		unsigned int vertexCount = 0;
+
+		for (const Mesh& mesh : model.m_meshes)
+		{
+			for (const glm::vec3& p : mesh.positions)
+				barycenter += p;
+			vertexCount += mesh.positions.size();
+		}
+		barycenter /= vertexCount;
+
+		return barycenter;
+	}
+
+	void ComputeEigenVectors(const Model& model, glm::vec3& eVec1, glm::vec3& eVec2, glm::vec3& eVec3, glm::vec3& eValues)
+	{
+		glm::vec3 barycenter = ComputeBarycenter(model);
+
+		Eigen::Matrix3d covariance;
+		covariance.setZero();
+		unsigned int vertexCount = 0;
+		for (const Mesh& mesh : model.m_meshes)
+		{
+			for (const glm::vec3& p : mesh.positions)
+			{
+				glm::vec3 dev = p - barycenter;
+				Eigen::Vector3d eigenDev = Eigen::Vector3d{ dev.x, dev.y, dev.z };
+				covariance += eigenDev * eigenDev.transpose();
+			}
+			vertexCount += mesh.positions.size();
+		}
+		covariance /= (double)vertexCount;
+
+		// Compute eigenvectors for the covariance matrix
+		Eigen::EigenSolver<Eigen::Matrix3d> solver(covariance);
+		Eigen::Matrix3d eigenVectors = solver.eigenvectors().real();
+		Eigen::Vector3d eigenValues = solver.eigenvalues().real();
+
+		eVec1 = glm::vec3(eigenVectors.row(0).x(), eigenVectors.row(0).y(), eigenVectors.row(0).z());
+		eVec2 = glm::vec3(eigenVectors.row(1).x(), eigenVectors.row(1).y(), eigenVectors.row(1).z());
+		eVec3 = glm::vec3(eigenVectors.row(2).x(), eigenVectors.row(2).y(), eigenVectors.row(2).z());
+		eValues = glm::vec3(eigenValues.x(), eigenValues.y(), eigenValues.z());
+	}
+}
---
+++ Source/ModelUtil.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glm/fwd.hpp>
+
+class Model;
+
+namespace util
+{
+	glm::vec3 ComputeBarycenter(const Model& model);
+
+	void ComputeEigenVectors(const Model& model, glm::vec3& eVec1, glm::vec3& eVec2, glm::vec3& eVec3, glm::vec3& eValues);
+}
