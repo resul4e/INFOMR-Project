79fb63712f7b387dba8e66102403693b38479ce9
Julian Thijssen <Nimthora@gmail.com>
2020-09-29 16:05:46 +0200

    Implement custom eigen vector rotation of model in model utilities

--- Source/ModelUtil.cpp
+++ Source/ModelUtil.cpp
@@ -51,4 +51,31 @@ namespace util
 		eVec3 = glm::vec3(eigenVectors.row(2).x(), eigenVectors.row(2).y(), eigenVectors.row(2).z());
 		eValues = glm::vec3(eigenValues.x(), eigenValues.y(), eigenValues.z());
 	}
+
+	void RotateMajorEigenVectorToXAxis(Model& model)
+	{
+		glm::vec3 eigenVectors[3];
+		glm::vec3 eigenValues;
+		util::ComputeEigenVectors(model, eigenVectors[0], eigenVectors[1], eigenVectors[2], eigenValues);
+
+		std::vector<int> sortedIndices = { 0, 1, 2 };
+		std::vector<float> sortedEigenValues = { eigenValues.x, eigenValues.y, eigenValues.z };
+		std::sort(std::begin(sortedIndices), std::end(sortedIndices), [&](int i1, int i2) { return sortedEigenValues[i1] > sortedEigenValues[i2]; });
+
+		glm::vec3 majorEigenVector = eigenVectors[sortedIndices[0]];
+		glm::vec3 medianEigenVector = eigenVectors[sortedIndices[1]];
+		glm::vec3 minorEigenVector = glm::cross(majorEigenVector, medianEigenVector);
+		
+		for (Mesh& mesh : model.m_meshes)
+		{
+			for (glm::vec3& p : mesh.positions)
+			{
+				float x = glm::dot(p, majorEigenVector);
+				float y = glm::dot(p, medianEigenVector);
+				float z = glm::dot(p, minorEigenVector);
+
+				p = glm::vec3(x, y, z);
+			}
+		}
+	}
 }
--- Source/ModelUtil.h
+++ Source/ModelUtil.h
@@ -9,4 +9,6 @@ namespace util
 	glm::vec3 ComputeBarycenter(const Model& model);
 
 	void ComputeEigenVectors(const Model& model, glm::vec3& eVec1, glm::vec3& eVec2, glm::vec3& eVec3, glm::vec3& eValues);
+
+	void RotateMajorEigenVectorToXAxis(Model& model);
 }
