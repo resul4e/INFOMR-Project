77eee76459c05111af868f5a02a31d0c7023d7cb
Julian Thijssen <Nimthora@gmail.com>
2020-09-29 16:05:46 +0200

    Add ModelDescriptor struct

---
+++ Source/ModelDescriptor.cpp
@@ -0,0 +1,30 @@
+#include "ModelDescriptor.h"
+
+#include "FeatureExtraction.h"
+#include "ModelUtil.h"
+
+# define M_PI           3.14159265358979323846  /* pi */
+
+void ModelDescriptor::UpdateFeatures()
+{
+	if (m_model != nullptr)
+	{
+		m_model->UpdateBounds();
+		m_model->CalculateOBB();
+	}
+	
+	m_3DFeatures.surfaceArea = ExtractSurfaceArea(*m_model);
+	m_3DFeatures.volume = ExtractVolume(*m_model);
+	m_3DFeatures.boundsArea = ExtractAABBArea(*m_model);
+	m_3DFeatures.boundsVolume = ExtractAABBVolume(*m_model);
+	m_3DFeatures.compactness = (std::pow(M_PI, 1.0 / 3.0) * std::pow((6.0 * m_3DFeatures.volume), 2.0 / 3.0)) / m_3DFeatures.surfaceArea;
+	glm::vec3 eigenValues{ 0 };
+	util::GetSortedEigenValues(*m_model, eigenValues);
+	m_3DFeatures.eccentricity = eigenValues.x / eigenValues.z;
+
+	ExtractA1(*m_model);
+	ExtractD1(*m_model);
+	ExtractD2(*m_model);
+	ExtractD3(*m_model);
+	ExtractD4(*m_model);
+}
---
+++ Source/ModelDescriptor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Model.h"
+
+#include <filesystem>
+#include <memory>
+
+struct Features3D
+{
+	Bounds bounds;
+	float volume;
+	float surfaceArea;
+	float compactness;
+	float boundsArea;
+	float boundsVolume;
+	float eccentricity;
+};
+
+struct ModelDescriptor
+{
+	void UpdateFeatures();
+
+	std::string m_name;
+	std::filesystem::path m_path;
+
+	std::shared_ptr<Model> m_model;
+
+	std::string m_class;
+	//size_t m_vertexCount;
+	//size_t m_faceCount;
+
+	Features3D m_3DFeatures;
+};
