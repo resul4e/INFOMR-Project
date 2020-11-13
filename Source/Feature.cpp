abd7b99034f6c87204feb7f4f1f6efec3cdaf9ee
Julian Thijssen <Nimthora@gmail.com>
2020-11-13 18:39:29 +0100

    Compute feature vector distances more cleanly

--- Source/Feature.cpp
+++ Source/Feature.cpp
@@ -101,19 +101,22 @@ float FeatureVectorDistance(const FeatureVector& fv1, const FeatureVector& fv2, 
 	EuclideanDistance euclidean;
 	WassersteinDistance wasserstein;
 	
-	float dist1 = euclidean.distance(fv1.m_features[0], fv2.m_features[0]);
-	//float dist2 = euclidean.distance(fv1.m_features[1], fv2.m_features[1]);
-	
-	//float dist2 = euclidean.distance(fv1.m_features[1], fv2.m_features[1]);
-	//float dist3 = euclidean.distance(fv1.m_features[2], fv2.m_features[2]);
-	//float dist4 = euclidean.distance(fv1.m_features[3], fv2.m_features[3]);
-	//float dist5 = euclidean.distance(fv1.m_features[4], fv2.m_features[4]);
-	//float dist6 = euclidean.distance(fv1.m_features[5], fv2.m_features[5]);
-	float dist7 = wasserstein.distance(fv1.m_histogramFeatures[0], fv2.m_histogramFeatures[0]) * hist_weights[0];
-	float dist8 = wasserstein.distance(fv1.m_histogramFeatures[1], fv2.m_histogramFeatures[1]) * hist_weights[1];
-	float dist9 = wasserstein.distance(fv1.m_histogramFeatures[2], fv2.m_histogramFeatures[2]) * hist_weights[2];
-	float dist10 = wasserstein.distance(fv1.m_histogramFeatures[3], fv2.m_histogramFeatures[3]) * hist_weights[3];
-	float dist11 = wasserstein.distance(fv1.m_histogramFeatures[4], fv2.m_histogramFeatures[4]) * hist_weights[4];
+	float singleDists = 0;
+	float histDists = 0;
 
-	return dist1 * 2 + (dist7 + dist8 + dist9 + dist10 + dist11);
+	for (int i = 0; i < fv1.m_features.size(); i++)
+	{
+		const Feature& f1 = fv1.m_features[i];
+		const Feature& f2 = fv2.m_features[i];
+		singleDists += euclidean.distance(f1, f2);
+	}
+
+	for (int i = 0; i < fv1.m_histogramFeatures.size(); i++)
+	{
+		const HistogramFeature& f1 = fv1.m_histogramFeatures[i];
+		const HistogramFeature& f2 = fv2.m_histogramFeatures[i];
+		histDists += wasserstein.distance(f1, f2) * hist_weights[i];
+	}
+
+	return singleDists * 2 + histDists;
 }
