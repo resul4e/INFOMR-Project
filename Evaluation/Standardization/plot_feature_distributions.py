4b28b7d1d8efef6b00aac9121247c239846e814e
Julian Thijssen <Nimthora@gmail.com>
2020-11-17 17:58:11 +0100

    Update standardization analytics script

--- Evaluation/Standardization/plot_feature_distributions.py
+++ Evaluation/Standardization/plot_feature_distributions.py
@@ -10,21 +10,20 @@ bounds_area = np.loadtxt('bounds_area_norm.csv', dtype='float', delimiter=',')
 bounds_volume = np.loadtxt('bounds_volume_norm.csv', dtype='float', delimiter=',')
 eccentricity = np.loadtxt('eccentricity_norm.csv', dtype='float', delimiter=',')
 
-def ComputeDistribution(values):
+def ComputeDistribution(featureName, values):
     mean = values[0]
     stddev = values[1]
     values = np.divide(np.subtract(values[2:], mean), stddev)
 
     _ = plt.hist(values, bins=np.linspace(-3, 3, 35), rwidth=0.9)
-    plt.title("Volume feature values standardized over database")
+    plt.title(featureName + " Feature Values Standardized over Database")
     plt.xlabel('Standard deviations')
     plt.ylabel("Number of shapes")
     plt.show()
 
-ComputeDistribution(volumes)
-ComputeDistribution(surface_area)
-ComputeDistribution(compactness)
-ComputeDistribution(bounds)
-ComputeDistribution(bounds_area)
-ComputeDistribution(bounds_volume)
-ComputeDistribution(eccentricity)
+ComputeDistribution("Volume", volumes)
+ComputeDistribution("Surface Area", surface_area)
+ComputeDistribution("Compactness", compactness)
+ComputeDistribution("AABB Area", bounds_area)
+ComputeDistribution("AABB Volume", bounds_volume)
+ComputeDistribution("Eccentricity", eccentricity)
